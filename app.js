2c9d25e43dfa68071f5314c88384d0f55814de4a
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-03-09 22:01:49 +0100

    added body parser to handle JSON

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const request = require('request');
+const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
+
 const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
@@ -9,4 +14,6 @@ app.listen(PORT, () => {
 
 app.post('/submit', function (req, res) {
     console.log("Hello from submit");
+    console.log(req.body);
+   // console.log(req);
 });
